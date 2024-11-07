330bd176d538d86544b212bfc87b6997debd4f51
Abhi Gupta <abhiguptaofficial30@gmail.com>
2024-11-07 23:10:31 +0530

    update app.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url="https://food-delivery-app-backend-hkft.onrender.com"
+  const url="https://food-delivery-app-backend-hkft.onrender.com".trimEnd('/')
 
   return (
     <div>
