d12992078eaeca3d6dddf18f9cd30fdaeed48339
Abhi Gupta <abhiguptaofficial30@gmail.com>
2024-11-07 23:12:35 +0530

    update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const [cartItems, setCartItems]=useState({});
-    const url="https://food-delivery-app-backend-hkft.onrender.com"
+    const url="https://food-delivery-app-backend-hkft.onrender.com".trimEnd('/')
     const [token, setToken]=useState("");
     const [food_list,setFoodList]=useState([])
 
