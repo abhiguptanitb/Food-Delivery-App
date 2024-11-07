90dd05582543a19b6a6763ea2ba5a4fdf484a935
Abhi Gupta <abhiguptaofficial30@gmail.com>
2024-11-07 22:14:30 +0530

    update app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home'
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
-import LoginPopUp from "./components/LogInPopup/LoginPopup";
+import LoginPopUp from "./components/LoginPopup/LoginPopup";
 import Verify from "./pages/Verify/verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
 
