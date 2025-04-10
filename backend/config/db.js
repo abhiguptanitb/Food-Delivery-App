622d18da1fd112ca7e22ea4bb0552ccb1561d84d
Abhi Gupta <abhiguptaofficial30@gmail.com>
2025-04-10 21:53:31 +0530

    updated db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://abhiguptaofficial30:fooddelivery@cluster0.roxrr.mongodb.net/food-delivery").then(()=>console.log('DB Connected'));
+    await mongoose.connect(process.env.DB_CONNECT).then(()=>console.log('DB Connected'));
 }
